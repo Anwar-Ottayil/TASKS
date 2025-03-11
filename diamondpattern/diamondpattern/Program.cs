4bca571935dfea024663cfb1bc396ca819cfc217
ANWAR OTTAYIL <anwarkpr964@gmail.com>
2025-03-11 09:47:50 +0530

    after pattern

---
+++ diamondpattern/diamondpattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class DiamondPattern
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of rows for the diamond: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i += 2)
+        {
+            PrintSpaces((n - i) / 2);
+            PrintStars(i);
+        }
+
+        for (int i = n - 2; i >= 1; i -= 2)
+        {
+            PrintSpaces((n - i) / 2);
+            PrintStars(i);
+        }
+    }
+
+    static void PrintSpaces(int count)
+    {
+        for (int i = 0; i < count; i++)
+            Console.Write(" ");
+    }
+
+    static void PrintStars(int count)
+    {
+        for (int i = 0; i < count; i++)
+            Console.Write("*");
+        Console.WriteLine();
+    }
+}
