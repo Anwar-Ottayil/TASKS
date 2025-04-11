2e056a70ab0efc7bde99b3d7145ce2462a5f8b81
ANWAR OTTAYIL <anwarkpr964@gmail.com>
2025-04-11 20:12:53 +0530

    entity crud

---
+++ DotNetWeek4/CRUD_ENTITY_FRAMEWORK/CRUD_ENTITY_FRAMEWORK/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using CRUD_ENTITY_FRAMEWORK.Data;
+using CRUD_ENTITY_FRAMEWORK.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_ENTITY_FRAMEWORK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return Ok(employees);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+                return NotFound();
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = employee.Id }, employee);
+            //return Ok(employee);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Employee updatedData)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null) return NotFound();
+
+            employee.Name = updatedData.Name;
+            employee.Salary = updatedData.Salary;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+    }
+
+}
+    
---
+++ DotNetWeek4/CRUD_ENTITY_FRAMEWORK/CRUD_ENTITY_FRAMEWORK/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+using CRUD_ENTITY_FRAMEWORK.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CRUD_ENTITY_FRAMEWORK.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
